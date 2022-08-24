453c7f950d287785109bf4263113ec9a1dd98de4
Kovács Enikő <67015358+kovacseni@users.noreply.github.com>
2022-08-24 16:34:35 +0200

    Update Conversion.java

--- solutions/typeconversion/src/main/java/typeconversion/Conversion.java
+++ solutions/typeconversion/src/main/java/typeconversion/Conversion.java
@@ -9,7 +9,7 @@ public class Conversion {
     public byte[] convertIntArrayToByteArray(int[] numbers) {
         byte[] result = new byte[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > 0 && numbers[i] < 127) {
+            if (numbers[i] >= 0 && numbers[i] <= 127) {
                 result[i] = (byte) numbers[i];
             } else {
                 result[i] = -1;
