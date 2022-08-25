8964a23cfb99eb774688cfae2cd63f2a5187ebe9
Kovács Enikő <67015358+kovacseni@users.noreply.github.com>
2022-08-25 16:51:58 +0200

    Update Codes.java

--- solutions/stringseparate/src/main/java/stringseparate/Codes.java
+++ solutions/stringseparate/src/main/java/stringseparate/Codes.java
@@ -1,20 +1,34 @@
 package stringseparate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Codes {
 
     public String getCodesStartWithLetter(List<String> codes) {
+        List<String> codesStartWithLetter = filterCodesStartsWithLetter(codes);
         StringBuilder sb = new StringBuilder("Betűvel kezdődő kódok: ");
-        for (int i = 0; i < codes.size(); i++) {
-            char first = codes.get(i).charAt(0);
-            if (Character.isAlphabetic(first) && i < codes.size() - 1) {
-                sb.append(codes.get(i));
+        for (int i = 0; i < codesStartWithLetter.size(); i++) {
+            String code = codesStartWithLetter.get(i);
+            char first = code.charAt(0);
+            if (i < codesStartWithLetter.size() - 1) {
+                sb.append(code);
                 sb.append(", ");
-            } else if (Character.isAlphabetic(first) && i == codes.size() - 1) {
-                sb.append(codes.get(i));
+            } else {
+                sb.append(code);
             }
         }
         return sb.toString();
+    }
+
+    private List<String> filterCodesStartsWithLetter(List<String> codes) {
+        List<String> codesStartWithLetter = new ArrayList<>();
+        for (String code : codes) {
+            char first = code.charAt(0);
+            if (Character.isAlphabetic(first)) {
+                codesStartWithLetter.add(code);
+            }
+        }
+        return codesStartWithLetter;
     }
 }
